c6fb287d105035432fd71ad2a9e107c47a036cd4
AnthonyIreo <baosheng@buffalo.edu>
2023-04-16 16:03:09 -0400

    create UI for speech screen

---
+++ speech_translation/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  speech_translation
+//
+//  Created by Bayson on 4/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var language: UILabel!
+    
+    
+    @IBOutlet weak var mic: UIButton!
+    
+    @IBOutlet weak var input: UILabel!
+    @IBOutlet weak var result: UILabel!
+    @IBOutlet weak var submit: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        input.layer.cornerRadius = 5;
+        input.layer.masksToBounds = true;
+        input.layer.borderWidth = 1;
+        result.layer.cornerRadius = 5;
+        result.layer.masksToBounds = true;
+        result.layer.borderWidth = 1;
+    }
+
+
+}
+
